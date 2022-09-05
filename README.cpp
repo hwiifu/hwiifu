2e5ecf2f0e2e62e47a8aac0e1dce8c18d4bd98b6
hwiifu <112888579+hwiifu@users.noreply.github.com>
2022-09-05 22:12:58 +0700

    Create README.cpp

---
+++ README.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x, y, z;
+    x=10, y=100, z=1000;
+    float pi=3.14;
+    char Grade = 'A';
+    //printf the values
+    printf("Sum of x, y and z is: %d\n", x+y+z);
+    printf("Then multiple by pi, we have: %f\n", (x+y+z)*pi);
+    printf("My Grade is: %c,\n", Grade);
+    return 0;
+}
